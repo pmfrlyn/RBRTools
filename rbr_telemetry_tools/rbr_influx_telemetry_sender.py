4906d2f8bf7c6c8659fb44cb99b8a6a9a3eeb63d
Evelyn Klein <evelykay@gmail.com>
2023-12-30 13:46:41 -0800

    Telemetry Repeater Client
    - Added support for
        - session tagging: on startup the client creates a datetime stamp
        that is then used to tag the session for that client lifetime
        - pause and restart within a stage. the client now tracks whether a
        stage has been paused or restarted
        - client now tags data with which attempt at the stage we are on
        - stage name detection and tagging

--- rbr_telemetry_tools/rbr_influx_telemetry_sender.py
+++ rbr_telemetry_tools/rbr_influx_telemetry_sender.py
@@ -1,10 +1,16 @@
-import influxdb_client
+import datetime
 import asyncio
+import async_timeout
 import socket
+
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 
+import influxdb_client
 
-from rbr_telemetry_tools import MESSAGE_LENGTH, FORMAT_FIELDS, process_telemetry_packet
+
+from rbr_telemetry_tools import (
+    MESSAGE_LENGTH, FORMAT_FIELDS, MAPS_INDEX,
+    process_telemetry_packet, configure_map_index)
 
 RBR_INSTALL_LOCATION = "D:\\Richard Burns Rally"
 RBR_MAPS_INI = RBR_INSTALL_LOCATION + "\\Maps\\tracks.ini"
@@ -19,15 +25,34 @@ driver = "spots"
 UDP_IP = "10.0.0.30"
 UDP_PORT = 6776
 
-async def rbr_telemetry_send(data_queue: asyncio.Queue):
-    data = await data_queue.get()
-    point = process_telemetry_packet(data, unit="C")
+current_stage = None
+current_total_steps = None
+stage_attempt = 1
 
-    tag_fields = ("stage.index", "car.index")
+paused_stage = None
+paused_total_steps = None
+
+session_tag = str(datetime.datetime.now())
+
+configure_map_index(RBR_MAPS_INI)
+
+async def rbr_telemetry_send(data_queue: asyncio.Queue):
+    try:
+        async with async_timeout.timeout(1):
+            point = await data_queue.get()
+    except asyncio.exceptions.TimeoutError:
+        return
+
+    tag_fields = ("stage.index", "car.index", "stage.name")
+
+    del point["general.total_steps"]
+
     async with InfluxDBClientAsync(url=url, token=token, org=org) as client:
         write_api = client.write_api()
         influx_point = influxdb_client.Point("RBR_RUN")
         influx_point.tag("driver", driver)
+        influx_point.tag("session_id", session_tag)
+        influx_point.tag("stage.attempt", stage_attempt)
 
         for tag in tag_fields:
             influx_point.tag(tag, point[tag])
@@ -40,26 +65,62 @@ async def rbr_telemetry_send(data_queue: asyncio.Queue):
         await write_api.write(bucket="rbrtelemetry", record=influx_point)
 
 async def rbr_telemetry_client(host, port, data_queue: asyncio.Queue):
+    global current_stage
+    global current_total_steps
+    global paused_stage
+    global paused_total_steps
+    global stage_attempt
+
     sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
     sock.bind((UDP_IP, UDP_PORT))
     
-    data, address = sock.recvfrom(664)
-    await data_queue.put(data)
-    sock.close()
+    point = None
+
+    sock.settimeout(1)
+    try:
+        data, _ = sock.recvfrom(664)
+        point = process_telemetry_packet(data, unit="C")
+
+        current_stage = point['stage.index']
+        current_total_steps = point['general.total_steps']
+
+        # handle the paused state and resume with the correct data updates
+        if paused_stage and paused_total_steps:
+            if paused_stage == current_stage and paused_total_steps > current_total_steps:
+                print("Stage Restart!")
+                stage_attempt += 1
+            elif paused_stage == current_stage and paused_total_steps <= current_total_steps:
+                print("Resuming From Pause")
+            elif paused_stage != current_stage:
+                print("New Stage!")
+                stage_attempt = 1
+
+            paused_stage = paused_total_steps = None
+
+        await data_queue.put(point)
+    except socket.timeout:
+        if not(paused_stage and paused_total_steps):
+            paused_stage = current_stage
+            paused_total_steps = current_total_steps
+            print("Waiting for telemetry...")
+        else:
+            print("Paused - SS: {}, Steps: {}".format("{} ({})".format(MAPS_INDEX.get(paused_stage, paused_stage), paused_stage), paused_total_steps))
+    finally:
+        sock.close()
 
 async def main():
     data_queue = asyncio.Queue()
-    stopped = asyncio.Event()
 
     loop = asyncio.get_event_loop()
-    while not stopped.is_set():
-        try:            
-            await rbr_telemetry_client(UDP_IP, UDP_PORT, data_queue)
-            await rbr_telemetry_send(data_queue)
-        except KeyboardInterrupt:
-            stopped.set()
+    while True:
+        t1 = loop.create_task(
+                rbr_telemetry_client(UDP_IP, UDP_PORT, data_queue))
+        t2 = loop.create_task(rbr_telemetry_send(data_queue))
 
+        await t1
+        await t2
+        
     loop.stop()
 
 asyncio.run(main())
