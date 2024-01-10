9c62220e22ed66dd7ad1faa54d76debae1502490
Evelyn Klein <evelykay@gmail.com>
2024-01-10 13:25:26 -0800

    Make things a bit more configurable, enpretty the output,  add functions n' stuff

--- rbr_telemetry_tools/plot_run_data.py
+++ rbr_telemetry_tools/plot_run_data.py
@@ -13,53 +13,107 @@ client = influxdb_client.InfluxDBClient(
     org=org
 )
 
-query_api = client.query_api()
+MEASUREMENT = "RBR_RUN"
+BUCKET = "rbrtelemetry"
 
-query = """from(bucket: "rbrtelemetry")
-  |> range(start: -3h)
-  |> filter(fn: (r) => r["_measurement"] == "RBR_RUN")
-  |> filter(fn: (r) => r["session_id"] == "2024-01-09 21:30:06.141923")
-  |> filter(fn: (r) => r["stage.run"] == "1")
-  |> filter(fn: (r) => r["_field"] == "car.position_x" or r["_field"] == "car.position_y" or r["_field"] == "car.position_z")
-  |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
-  |> group(columns: ["stage.run_attempt"])"""
+SESSION_ID = "2024-01-09 21:30:06.141923"
+RUN_ID = 1
 
-result = query_api.query(org=org, query=query)
+def main():
+    plot_runs(
+        query_from_influx(SESSION_ID, RUN_ID, BUCKET, MEASUREMENT)
+    )
+
+def query_from_influx(session_id, run_id, bucket="rbrtelemetry", measurement="RBR_RUN", lookback_window="-1w"):
+    query = """from(bucket: "{bucket}")
+        |> range(start: {lookback_window})
+        |> filter(fn: (r) => r["_measurement"] == "{measurement}")
+        |> filter(fn: (r) => r["session_id"] == "{session_id}")
+        |> filter(fn: (r) => r["stage.run"] == "{run_id}")
+        |> filter(fn: (r) => r["_field"] == "car.position_x" or 
+                            r["_field"] == "car.position_y" or 
+                            r["_field"] == "car.position_z" or
+                            r["_field"] == "stage.distance_to_end" or 
+                            r["_field"] == "car.speed" or 
+                            r["_field"] == "stage.name")
+        |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
+        |> group(columns: ["stage.run_attempt"])""".format(
+            bucket=bucket,
+            lookback_window=lookback_window,
+            measurement=measurement,
+            session_id=session_id,
+            run_id=run_id,
+        )
+
+    query_api = client.query_api()
+    return query_api.query(org=org, query=query)
+
+def plot_runs(results):
+    run_vals = defaultdict(list)
+
+    for plot_table in results:
+        # positional values
+        x_vals = []
+        y_vals = []
+        z_vals = []
+
+        #time determination
+        start_time = None
+        end_time = None
+        prev_record = None
 
 
-run_vals = defaultdict(list)
+        for record in plot_table.records:
+            # extract start and end times for the stage
+            if prev_record and not start_time and float(record['car.speed']) > 0:
+                if float(prev_record['stage.distance_to_end']) > record['stage.distance_to_end']:
+                    start_time = prev_record['_time']
+                #    print("Stage Start Time: {}".format(start_time))
+                #else:
+                #    print("Ignoring speed wiggle")
+            
+            if not end_time and int(record['stage.distance_to_end']) <= 0:
+                end_time = record['_time']
+                #print("Stage End Time: {}".format(end_time))
+            
+            #print(record['stage.distance_to_end'])
+            x_vals.append(record['car.position_x'])
+            y_vals.append(record['car.position_y'])
+            z_vals.append(record['car.position_z'])
 
-for plot_table in result:
-    x_vals = []
-    y_vals = []
-    z_vals = []
+            prev_record = record
 
-    for record in plot_table.records:
-        x_vals.append(record['car.position_x'])
-        y_vals.append(record['car.position_y'])
-        z_vals.append(record['car.position_z'])
+        stage_time = end_time - start_time
+        label = "Attempt {} ({})".format(record['stage.run_attempt'], stage_time)
+        run_vals[label] = (x_vals, y_vals, z_vals, record['stage.name'])
 
-    run_vals["attempt {}".format(record['stage.run_attempt'])] = (x_vals, y_vals, z_vals)
+        print("Stage Time: {}".format(stage_time))
 
-import matplotlib.pyplot as plt
-from matplotlib import colormaps
+    import matplotlib.pyplot as plt
+    from matplotlib import colormaps
 
+    plt.style.use('_mpl-gallery')
 
-plt.style.use('_mpl-gallery')
+    # plot
+    fig, ax = plt.subplots()
 
-# plot
-fig, ax = plt.subplots()
+    ax.set_prop_cycle(color=colormaps["tab20"].colors)
 
-ax.set_prop_cycle(color=colormaps["tab20"].colors)
+    for label, vector_vals in run_vals.items():
+        # Add the path
+        ax.plot(vector_vals[0], vector_vals[1], linewidth=1.0, label = label)
+        # Add begin and end markers
+        ax.plot(vector_vals[0][0], vector_vals[1][0], 
+                markersize=5, marker="o", markerfacecolor="blue")
+        ax.plot(vector_vals[0][-1], vector_vals[1][-1], 
+                markersize=5, marker="o", markerfacecolor="red")
 
-for label, vector_vals in run_vals.items():
-    # Add the path
-    ax.plot(vector_vals[0], vector_vals[1], linewidth=1.0, label = label)
-    # Add begin and end markers
-    ax.plot(vector_vals[0][0], vector_vals[1][0], markersize=5, marker="o", markerfacecolor="blue")
-    ax.plot(vector_vals[0][-1], vector_vals[1][-1], markersize=5, marker="o", markerfacecolor="red")
+    ax.set_aspect('equal', adjustable="box")
 
-ax.set_aspect('equal', adjustable="box")
+    plt.subplots_adjust(top=0.97, bottom=0.03)
+    plt.legend()
+    plt.title(vector_vals[3])
+    plt.show()
 
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    main()
