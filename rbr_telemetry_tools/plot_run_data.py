bd9f7e0706dedaebf16941e2ea9fffe888eaa86c
Evelyn <spots@klaw.cloud>
2024-01-11 10:28:05 -0800

    Cleanup and gentle refactoring

--- rbr_telemetry_tools/plot_run_data.py
+++ rbr_telemetry_tools/plot_run_data.py
@@ -20,16 +20,18 @@ client = influxdb_client.InfluxDBClient(
 MEASUREMENT = "RBR_RUN"
 BUCKET = "rbrtelemetry"
 
-#SESSION_ID = "2024-01-09 21:30:06.141923"
-SESSION_ID = "2024-01-10 13:57:20.903919"
+SESSION_ID = "2024-01-09 21:30:06.141923"
+#SESSION_ID = "2024-01-10 13:57:20.903919"
 RUN_ID = 1
-PLOT_ARROWS = False
+PLOT_ARROWS = True
 DEBUG = False
 INFO = True
 
 def main():
     plot_runs(
-        query_from_influx(SESSION_ID, RUN_ID, BUCKET, MEASUREMENT)
+        process_run_results(
+            query_from_influx(SESSION_ID, RUN_ID, BUCKET, MEASUREMENT)
+        )
     )
 
 def log_debug(msg):
@@ -48,6 +50,7 @@ def query_from_influx(session_id, run_id, bucket="rbrtelemetry", measurement="RB
         |> filter(fn: (r) => r["_measurement"] == "{measurement}")
         |> filter(fn: (r) => r["session_id"] == "{session_id}")
         |> filter(fn: (r) => r["stage.run"] == "{run_id}")
+        |> filter(fn: (r) => r["stage.run_attempt"] == "2")
         |> filter(fn: (r) => r["_field"] == "car.position_x" or
                              r["_field"] == "car.position_y" or
                              r["_field"] == "car.position_z" or
@@ -68,7 +71,7 @@ def query_from_influx(session_id, run_id, bucket="rbrtelemetry", measurement="RB
     query_api = client.query_api()
     return query_api.query(org=org, query=query)
 
-def plot_runs(results):
+def process_run_results(results):
     run_vals = defaultdict(list)
 
     for plot_table in results:
@@ -83,6 +86,7 @@ def plot_runs(results):
         end_time = None
         prev_record = None
         points = 0
+        max_speed = 0
 
         for record in plot_table.records:
             points += 1
@@ -105,23 +109,26 @@ def plot_runs(results):
 
                 car_yaw = float(record["car.yaw"])
                 yaw_degrees = 360 - (180 + car_yaw)
-                yaw_vals.append(yaw_degrees)
+                yaw_vals.append((yaw_degrees, float(record["car.speed"])))
 
+                if float(record['car.speed']) > max_speed:
+                    max_speed = float(record['car.speed'])
 
             prev_record = record
 
         stage_time = end_time - start_time
         label = "Attempt {} ({})".format(record['stage.run_attempt'], stage_time)
-        run_vals[label] = (x_vals, y_vals, z_vals, yaw_vals, record['stage.name'])
+        run_vals[label] = (x_vals, y_vals, z_vals, yaw_vals, record['stage.name'], max_speed)
 
-        log_info("Retrieved Stage: {}, Attempt: {}, Data Points: {}, Stage Time: {}".format(
+        log_info("Retrieved Stage: {}, Attempt: {}, Data Points: {}, Stage Time: {}, Max Speed: {}".format(
                                                                     record['stage.name'],
                                                                     record['stage.run_attempt'],
-                                                                    points, stage_time))
+                                                                    points, stage_time,
+                                                                    max_speed))
+    return run_vals
 
-
+def plot_runs(run_vals):
     plt.style.use('_mpl-gallery')
-
     # plot
     fig, ax = plt.subplots()
 
@@ -151,20 +158,28 @@ def plot_runs(results):
         #Add some yaw angle arrows
         for label, vector_vals in run_vals.items():
             counter = 0
-            for x, y, yaw_degrees in zip(vector_vals[0], vector_vals[1], vector_vals[3]):
-                if counter % 20 != 0:
+            max_speed = vector_vals[5]
+            print("max_speed: ", max_speed)
+            for x, y, yaw_info in zip(vector_vals[0], vector_vals[1], vector_vals[3]):
+                if counter % 10 != 0:
                     counter += 1
                     continue
                 else:
                     counter += 1
 
+                yaw_degrees, car_speed = yaw_info
+
+                max_speed_pct = (0 if not car_speed else car_speed / max_speed) * 100
+
+                print("car_speed: ", car_speed, "max_speed_pct: ", max_speed_pct)
                 yaw_radians = (3.145 * yaw_degrees) / 180
 
-                x_head = x + (math.cos(yaw_radians) * 2)
-                y_head = y + (math.sin(yaw_radians) * 2)
+                x_head = x + (math.cos(yaw_radians) * 1)
+                y_head = y + (math.sin(yaw_radians) * 1)
 
                 arrow = mpatches.FancyArrowPatch((x, y), (x_head, y_head),
-                                                mutation_scale=5)
+                                                 mutation_scale=5,
+                                                 arrowstyle="simple")
                 ax.add_patch(arrow)
 
 
--- rbr_telemetry_tools/rbr_telemetry_tools/format.py
+++ rbr_telemetry_tools/rbr_telemetry_tools/format.py
@@ -3,14 +3,14 @@ from collections import namedtuple
 MESSAGE_LENGTH = 664
 
 FORMAT_INFO = (
-    ("<", ""),                         #little endian                    
+    ("<", ""),                         #little endian
     ("I", "general.total_steps"),
     ("I", "stage.index"),
     ("f", "stage.progress"),
     ("f", "stage.total_race_time"),    #seconds?
     ("f", "stage.drive_line_location"),
     ("f", "stage.distance_to_end"),    #meters or feet(?) depending on config
- 
+
     ("f", "control.steering_input"),
     ("f", "control.throttle_input"),
     ("f", "control.brake_input"),
@@ -19,7 +19,7 @@ FORMAT_INFO = (
     ("i", "control.current_gear"),
     ("f", "control.brake_pressure"),
     ("f", "control.handbrake_pressure"),
- 
+
     ("i", "car.index"),
     ("f", "car.speed"),                               #in mph or kmh depending on config
     ("f", "car.position_x"),
@@ -28,155 +28,155 @@ FORMAT_INFO = (
     ("f", "car.roll"),
     ("f", "car.pitch"),
     ("f", "car.yaw"),
- 
+
     ("f", "car.velocities.surge"),
     ("f", "car.velocities.sway"),
     ("f", "car.velocities.heave"),
     ("f", "car.velocities.roll"),
     ("f", "car.velocities.pitch"),
     ("f", "car.velocities.yaw"),
-    
+
     ("f", "car.accelerations.surge"),
     ("f", "car.accelerations.sway"),
     ("f", "car.accelerations.heave"),
     ("f", "car.accelerations.roll"),
     ("f", "car.accelerations.pitch"),
-    ("f", "car.accelerations.yaw"),       
-    
+    ("f", "car.accelerations.yaw"),
+
     ("f", "engine.rpm"),
     ("f", "engine.radiator_coolant_temperature"),       #kelvin
     ("f", "engine.engine_coolant_temperature"),         #kelvin
     ("f", "engine.engine_temperature"),                #kelvin
-    
+
     ("f", "suspension.lf.spring_deflection"),
     ("f", "suspension.lf.rollbar_force"),
     ("f", "suspension.lf.spring_force"),
     ("f", "suspension.lf.damper_force"),
     ("f", "suspension.lf.strut_force"),
     ("i", "suspension.lf.helper_spring_is_active"),
-    
+
     ("f", "suspension.lf.damper.damage"),
     ("f", "suspension.lf.damper.pistonvelocity"),
-    
+
     ("f", "suspension.lf.brake.layer_temperature"),    #kelvin?
     ("f", "suspension.lf.brake.temperature"),         #kelvin?
     ("f", "suspension.lf.brake.wear"),
-    
+
     ("f", "suspension.lf.tire.pressure"),
     ("f", "suspension.lf.tire.temperature"),
     ("f", "suspension.lf.tire.carcass_temperature"),
     ("f", "suspension.lf.tire.tread_temperature"),
     ("I", "suspension.lf.tire.current_segment"),
-    
+
     ("f", "suspension.lf.tire.segment1.temperature"),
     ("f", "suspension.lf.tire.segment1.wear"),
-    
+
     ("f", "suspension.lf.tire.segment2.temperature"),
     ("f", "suspension.lf.tire.segment2.wear"),
-    
+
     ("f", "suspension.lf.tire.segment3.temperature"),
     ("f", "suspension.lf.tire.segment3.wear"),
-    
+
     ("f", "suspension.lf.tire.segment4.temperature"),
     ("f", "suspension.lf.tire.segment4.wear"),
-    
+
     ("f", "suspension.lf.tire.segment5.temperature"),
     ("f", "suspension.lf.tire.segment5.wear"),
-    
+
     ("f", "suspension.lf.tire.segment6.temperature"),
     ("f", "suspension.lf.tire.segment6.wear"),
-    
+
     ("f", "suspension.lf.tire.segment7.temperature"),
     ("f", "suspension.lf.tire.segment7.wear"),
-    
+
     ("f", "suspension.lf.tire.segment8.temperature"),
     ("f", "suspension.lf.tire.segment8.wear"),
-    
+
     ("f", "suspension.rf.spring_deflection"),
     ("f", "suspension.rf.rollbar_force"),
     ("f", "suspension.rf.spring_force"),
     ("f", "suspension.rf.damper_force"),
     ("f", "suspension.rf.strut_force"),
     ("i", "suspension.rf.helper_spring_is_active"),
-    
+
     ("f", "suspension.rf.damper.damage"),
     ("f", "suspension.rf.damper.pistonvelocity"),
-    
+
     ("f", "suspension.rf.brake.layer_temperature "),
     ("f", "suspension.rf.brake.temperature"),
     ("f", "suspension.rf.brake.wear"),
-    
+
     ("f", "suspension.rf.tire.pressure"),
     ("f", "suspension.rf.tire.temperature"),
     ("f", "suspension.rf.tire.carcass_temperature"),
     ("f", "suspension.rf.tire.tread_temperature"),
     ("I", "suspension.rf.tire.current_segment"),
-    
+
     ("f", "suspension.rf.tire.segment1.temperature"),
     ("f", "suspension.rf.tire.segment1.wear"),
-    
+
     ("f", "suspension.rf.tire.segment2.temperature"),
     ("f", "suspension.rf.tire.segment2.wear"),
-    
+
     ("f", "suspension.rf.tire.segment3.temperature"),
     ("f", "suspension.rf.tire.segment3.wear"),
-    
+
     ("f", "suspension.rf.tire.segment4.temperature"),
     ("f", "suspension.rf.tire.segment4.wear"),
-    
+
     ("f", "suspension.rf.tire.segment5.temperature"),
     ("f", "suspension.rf.tire.segment5.wear"),
-    
+
     ("f", "suspension.rf.tire.segment6.temperature"),
     ("f", "suspension.rf.tire.segment6.wear"),
-    
+
     ("f", "suspension.rf.tire.segment7.temperature"),
     ("f", "suspension.rf.tire.segment7.wear"),
-    
+
     ("f", "suspension.rf.tire.segment8.temperature"),
     ("f", "suspension.rf.tire.segment8.wear"),
-    
+
     ("f", "suspension.lb.spring_deflection"),
     ("f", "suspension.lb.rollbar_force"),
     ("f", "suspension.lb.spring_force"),
     ("f", "suspension.lb.damper_force"),
     ("f", "suspension.lb.strut_force"),
     ("i", "suspension.lb.helper_spring_is_active"),
-    
+
     ("f", "suspension.lb.damper.damage"),
     ("f", "suspension.lb.damper.pistonvelocity"),
-    
+
     ("f", "suspension.lb.brake.layer_temperature"),
     ("f", "suspension.lb.brake.temperature"),
     ("f", "suspension.lb.brake.wear"),
-    
+
     ("f", "suspension.lb.tire.pressure"),
     ("f", "suspension.lb.tire.temperature"),
     ("f", "suspension.lb.tire.carcass_temperature"),
     ("f", "suspension.lb.tire.tread_temperature"),
     ("I", "suspension.lb.tire.current_segment"),
-    
+
     ("f", "suspension.lb.tire.segment1.temperature"),
     ("f", "suspension.lb.tire.segment1.wear"),
-    
+
     ("f", "suspension.lb.tire.segment2.temperature"),
     ("f", "suspension.lb.tire.segment2.wear"),
-    
+
     ("f", "suspension.lb.tire.segment3.temperature"),
     ("f", "suspension.lb.tire.segment3.wear"),
-    
+
     ("f", "suspension.lb.tire.segment4.temperature"),
     ("f", "suspension.lb.tire.segment4.wear"),
-    
+
     ("f", "suspension.lb.tire.segment5.temperature"),
     ("f", "suspension.lb.tire.segment5.wear"),
-    
+
     ("f", "suspension.lb.tire.segment6.temperature"),
     ("f", "suspension.lb.tire.segment6.wear"),
-    
+
     ("f", "suspension.lb.tire.segment7.temperature"),
     ("f", "suspension.lb.tire.segment7.wear"),
-    
+
     ("f", "suspension.lb.tire.segment8.temperature"),
     ("f", "suspension.lb.tire.segment8.wear"),
 
@@ -186,41 +186,41 @@ FORMAT_INFO = (
     ("f", "suspension.rb.damper_force"),
     ("f", "suspension.rb.strut_force"),
     ("i", "suspension.rb.helper_spring_is_active"),
-   
+
     ("f", "suspension.rb.damper.damage"),
     ("f", "suspension.rb.damper.pistonvelocity"),
-   
+
     ("f", "suspension.rb.brake.layer_temperature"),
     ("f", "suspension.rb.brake.temperature"),
     ("f", "suspension.rb.brake.wear"),
-   
+
     ("f", "suspension.rb.tire.pressure"),
     ("f", "suspension.rb.tire.temperature"),
     ("f", "suspension.rb.tire.carcass_temperature"),
     ("f", "suspension.rb.tire.tread_temperature"),
     ("I", "suspension.rb.tire.current_segment"),
-   
+
     ("f", "suspension.rb.tire.segment1.temperature"),
     ("f", "suspension.rb.tire.segment1.wear"),
-   
+
     ("f", "suspension.rb.tire.segment2.temperature"),
     ("f", "suspension.rb.tire.segment2.wear"),
-   
+
     ("f", "suspension.rb.tire.segment3.temperature"),
     ("f", "suspension.rb.tire.segment3.wear"),
-   
+
     ("f", "suspension.rb.tire.segment4.temperature"),
     ("f", "suspension.rb.tire.segment4.wear"),
-   
+
     ("f", "suspension.rb.tire.segment5.temperature"),
     ("f", "suspension.rb.tire.segment5.wear"),
-   
+
     ("f", "suspension.rb.tire.segment6.temperature"),
     ("f", "suspension.rb.tire.segment6.wear"),
-   
+
     ("f", "suspension.rb.tire.segment7.temperature"),
     ("f", "suspension.rb.tire.segment7.wear"),
-   
+
     ("f", "suspension.rb.tire.segment8.temperature"),
     ("f", "suspension.rb.tire.segment8.wear")
 )
