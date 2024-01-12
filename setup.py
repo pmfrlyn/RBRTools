41a72163c9b385dd8b51c8a1adbddde1ee27e91b
Evelyn Klein <evelykay@gmail.com>
2024-01-12 12:01:22 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
     entry_points={
         'console_scripts': [
             'rbr_run_plotter = rbr_run_plotter:cli',
+            'rbr_influx_telemetry_sender = rbr_influx_telemetry_sender:cli',
+            'rbr_setup_compare = rbr_setup_compare:main'
         ],
     },
 )
