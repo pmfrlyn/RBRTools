cbd2996ceb59d582b5649f87a1f1b03203fa6105
Evelyn <spots@klaw.cloud>
2024-01-30 18:26:15 -0800

    - last bit of setups lib testing. - add pytest as a requirement

--- tests/test_setups_parser.py
+++ tests/test_setups_parser.py
@@ -1,10 +1,15 @@
 import rbr_tools.setups.parser as lsparser
-from rbr_tools.setups import parse_lsp_file, CarSetup, SetupSection
+from rbr_tools.setups import parse_lsp_file, CarSetup, SetupSection, Vector
 
 import os.path
+import pytest
 
 LSP_PATH = os.path.join("samples", "clio_tarmac_qubits.lsp")
-                        
+
+@pytest.fixture
+def carsetup():
+    return parse_lsp_file(LSP_PATH)
+
 def test_parser_creation():
     the_parser = lsparser.lsp_parser()
     assert the_parser is not None
@@ -16,7 +21,17 @@ def test_parser_parses():
 
     assert isinstance(carsetup["Drive"], SetupSection)
 
-def test_parser_parses_floats():
-    carsetup = parse_lsp_file(LSP_PATH)
-    assert isinstance(carsetup, CarSetup)
+def test_parser_parses_floats(carsetup):
     assert isinstance(carsetup["Drive"]["CenterDiffMaxTorque"], float)
+
+def test_parser_parses_vectors(carsetup):
+    vector = carsetup["WheelRF"]["vecTopMountPosition"]
+
+    assert isinstance(
+        vector,
+        Vector
+    )
+
+    assert isinstance(vector.x, float)
+    assert isinstance(vector.y, float)
+    assert isinstance(vector.z, float)
